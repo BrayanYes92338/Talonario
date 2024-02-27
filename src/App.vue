cc1cb8b0c97cba7f72512c348c5f91929f67e28b
BrayanYes92338 <145297925+BrayanYes92338@users.noreply.github.com>
2024-02-27 16:21:25 -0500

    new update

--- src/App.vue
+++ src/App.vue
@@ -49,8 +49,11 @@
         <div class="grid">
           <div class="cont-cantboletas">
             <div v-for="(item, i) in arr" :key="i">
-              <button class="rounded-circle boleta" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal"
-                @click="traerDatos(item)">{{ item.numero }}</button>
+              <button class="rounded-circle boleta" :style="item.estado === 0 ? 'background-color: #014BAE'
+                : item.estado === 1 ? 'background-color: #CF1635'
+                  : item.estado === 2 ? 'background-color: #0090A9'
+                    : item.estado === 3 ? 'background-color: #BA8C34' : ''" type="button" data-bs-toggle="modal"
+                data-bs-target="#exampleModal" @click="traerDatos(item, i)">{{ item.numero }}</button>
             </div>
           </div>
         </div>
@@ -73,9 +76,12 @@
             <div class="modal-body">
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               <h3>Boleta {{ numsele }}</h3>
-              <div v-if="estado === 0">
+              <div v-if="estado === 0" class="cen">
                 <h6 class="rowc">Estado: Disponible<div class="color" style="background-color: #00075F;"></div>
                 </h6>
+                <hr v-if="estado === 0">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">🤝
+                  Adquirir Boleta </button>
               </div>
               <div v-if="estado === 1">
                 <h6 class="rowc">Estado: Apartado <div class="color" style="background-color: #CF1635;"></div>
@@ -89,15 +95,24 @@
                 <h6 class="rowc">Estado: Ganador <div class="color" style="background-color: #BA8C34;"></div>
                 </h6>
               </div>
-              <hr>
-              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop2">🤝
-                Adquirir Boleta </button>
+              <div v-if="estado !== 0" class="cen">
+                <hr>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#participante">👁‍🗨
+                  Ver datos del participante </button>
+                <br v-if="estado !== 3">
+                <button v-if="estado !== 3" type="button" class="btn btn-primary" @click="libb()">❌ Liberar Boleta
+                </button>
+                <br v-if="estado !== 2 && estado !== 3">
+                <button v-if="estado === 1" type="button" class="btn btn-primary" @click="pagar()"> ✔ Pagar Boleta
+                </button>
+              </div>
+
             </div>
           </div>
         </div>
       </div>
-      <div class="modal fade prueba-color" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+      <div class="modal fade prueba-color" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false"
+        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content contenido">
             <div class="modal-header ">
@@ -107,6 +122,7 @@
             <div class="modal-body">
               <input type="text" placeholder="Ingrese nombre del comprador" v-model="nombreC">
               <input type="text" placeholder="Ingrese direccion del comprador" v-model="direccionC">
+              <input type="number" placeholder="Ingrese numero telefonico " v-model="telefonoC">
               <input type="date" v-model="fechaC">
               <select v-model="estadoC">
                 <option disabled selected hidden value="">Seleccione el estado de la boleta</option>
@@ -115,6 +131,28 @@
                 <option value="3">Ganador</option>
               </select>
               <button class="btn btn-primary botoncito" @click="regBoletas()">Registrar</button>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="modal fade" id="participante" tabindex="-1" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-bottom">
+          <div class="modal-content">
+            <div class="modal-header ">
+              <h1 class="modal-title fs-5 titulo-datos-boleta" id="exampleModalLabel2">Participante</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+
+              <span>{{ nombreP }}</span>
+              <span>{{ direccionP }}</span>
+              <span>{{ telefonoP }}</span>
+              <span>{{ fechaP}}</span>
+              <span v-if="estado === 1">Sin Pagar</span>
+              <span v-if="estado === 2">Pagado</span>
+              <span v-if="estado === 3">Ganador</span>
 
             </div>
           </div>
@@ -143,13 +181,19 @@ let fecha = ref("");
 let error = ref("");
 let edit = true;
 let index = null;
+let i = ref(0)
 let numsele = ref(0)
 let estado = ref(0)
 let nombreC = ref("")
 let direccionC = ref("")
+let telefonoC = ref("")
 let fechaC = ref("")
 let estadoC = ref("");
-let error2 = ref("");
+
+let nombreP = ref("")
+let direccionP = ref("")
+let telefonoP = ref("")
+let fechaP = ref("")
 
 
 
@@ -158,16 +202,39 @@ function regBoletas() {
   const cliente = {
     nombre: nombreC.value,
     direccion: direccionC.value,
+    telefono: telefonoC.value,
     fecha: fechaC.value,
     estado: parseInt(estadoC.value)
   }
+
   registros.value.push(cliente)
-  console.log(registros.value);
+
+  arr.value[i.value].comprador = cliente
+  console.log(arr.value);
+  arr.value[i.value].estado = parseInt(estadoC.value)
+
 }
 
-function traerDatos(item) {
+function traerDatos(item, index) {
   numsele.value = item.numero
   estado.value = item.estado
+  i.value = index
+
+  nombreP.value = item.comprador.nombre
+  direccionP.value = item.comprador.direccion
+  telefonoP.value = item.comprador.telefono
+  fechaP.value = item.comprador.fecha
+}
+
+function libb() {
+
+  arr.value[i.value].estado = 0
+  arr.value[i.value].comprador = {}
+}
+
+function pagar() {
+  arr.value[i.value].estado = 2
+
 }
 
 
@@ -317,7 +384,8 @@ function agregar() {
 
   arr.value = Array.from({ length: cantboletas.value }, (value, index) => ({
     numero: index,
-    estado: 0
+    estado: 0,
+    comprador: {}
   }));
 
   console.log(arr.value);
